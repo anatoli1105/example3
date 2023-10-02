4100219eb0b75f9d528f8500f574e9d2598595d6
anatolii <anatoliise@mail.ru>
2023-10-02 23:53:02 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,19 @@ public class Main {
         return x+y;
 
     }
+    public void maxNumbersClas(int x,int y){
+        if(x>y){ System.out.println ("число x больше числа y");}
+        else {System.out.println("\"число y больше числа x");}
+    }
 
     public static void main(String[] args) {
         int firstNumber=4;
         int secondNumber=5;
         Main sumNumbers=new Main();
-        System.out.println(sumNumbers.sumNumbersClas(firstNumber,secondNumber));
+        //System.out.println(sumNumbers.sumNumbersClas(firstNumber,secondNumber));
+        Main maxNumbers=new Main();
+        maxNumbers.maxNumbersClas(firstNumber,secondNumber);
+
 
     }
 }
